c49e73a3c665e8461b3d59dd667b7bf9668529f2
plusmancn <plusmancn@gmail.com>
2017-04-20 12:31:35 +0800

    feat: 实现 GetRow API

--- test/api.js
+++ test/api.js
@@ -2,6 +2,7 @@
 /**
  * API 测试
  */
+const should = require('should');
 const OTS = require('../');
 let ots = new OTS({
     endPoint: 'http://pl-msgc.cn-hangzhou.ots.aliyuncs.com',
@@ -12,60 +13,39 @@ let ots = new OTS({
 
 describe('#api', function () {
     describe('#PutRow', function() {
-        it('Test: ', function(done) {
+        it('Put Col0, Col1 With returnType RT_NONE', function(done) {
             ots.PutRow('sampleTable', {
                 'pk': 'pkValue'
             }, {
                 'Col0': 19,
-                'Col1': 'test'
+                'Col1': 'test value 中文值2'
+            }, {
+                returnContent: {
+                    returnType: 'RT_NONE'
+                }
+            }, function (err, result) {
+                should.ifError(err);
+                should.equal(result.consumed.capacityUnit.write, 1, 'capacityUnit.write shoud be 1');
+                done();
             });
         });
     });
 
-    /**
-Table 值
-0a:0b:73:61:6d:70:6c:65:54:61:62:6c:65:12:69:
-
-PK 值
-75:00:00:00:01:
-03:04:02:00:00:00:70:6b:
-05:0c:00:00:00:03:07:00:00:00:70:6b:56:61:6c:75:65:0a:85:
-
-ATTR 值
-02:
-03:04:04:00:00:00:43:6f:6c:30:05:09:00:00:00:00:13:00:00:00:
-00:00:00:00:
-07:46:ff:90:81:5b:01:00:00:
-
-CELL checksum
-0a:10:
-
-03:04:04:00:00:00:43:6f:6c:31:05:09:00:00:00:03:04:00:00:00:74:65:73:74
-
-:07:46:ff:90:81:5b:01:00:00:
-
-CELL checksum
-0a:6b
-
-ROW checksum
-:09:bf:
-
-
-1a:02:08:00:22:02:08:00
-
-         */
-
-    // describe('#GetRow', function() {
-    //     it('Test: pk, pkValue', function(done) {
-    //         ots.GetRow('sampleTable', [
-    //             {
-    //                 name: 'pk',
-    //                 value: 'pkValue'
-    //             }
-    //         ], {
-    //             maxVersions: 1
-    //         });
-    //         done();
-    //     });
-    // });
+    describe('#GetRow', function() {
+        it('Get Row identify by pk with pkValue', function(done) {
+            ots.GetRow('sampleTable',{
+                'pk': 'pkValue'
+            }, {
+                maxVersions: 1
+            }, function (err, result) {
+                should.ifError(err);
+                should.equal(result.consumed.capacityUnit.read, 1, 'capacityUnit.read shoud be 1');
+                should.deepEqual(result.rowDecode, {
+                    pk: { pk: 'pkValue' },
+                    attr: { Col0: 19, Col1: 'test value 中文值2' }
+                });
+                done();
+            });
+        });
+    });
 });
