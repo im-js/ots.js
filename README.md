4a8be836513bf0baa2e84d7994f93bfb3892ec75
plusmancn <plusmancn@gmail.com>
2017-04-21 01:10:29 +0800

    feat:  完成 GetRange API

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ots.js
-> 开发中，基于 2015-12-31 版 API，支持 PlainBuffer，和官方最新版 java-sdk 保持同步。
+> 开发中(50%)，基于 2015-12-31 版 API，支持 PlainBuffer，和官方最新版 java-sdk 保持同步。
+
+## Feature
+* PlainBuffer Support
+* Base on 2015-12-31 API (latest API so far)
+* AutoIncrement PrimaryKey Support
--- test/api.js
+++ test/api.js
@@ -12,13 +12,20 @@ let ots = new OTS({
 });
 
 describe('#api', function () {
+    describe('#Symbol', function () {
+        it('ensure INF_MAX, INF_MIN symbol Equality', function () {
+            should.deepEqual(ots.INF_MAX, OTS.INF_MAX);
+            should.deepEqual(ots.INF_MIN, OTS.INF_MIN);
+        });
+    });
+    
     describe('#PutRow', function() {
-        it('Put Col0, Col1 With returnType RT_NONE', function(done) {
+        it('Put row with pk=pkvalue, attr Col0, Col1 With returnType RT_NONE', function(done) {
             ots.PutRow('sampleTable', {
-                'pk': 'pkValue'
+                pk: 'pkValue'
             }, {
-                'Col0': 19,
-                'Col1': 'test value 中文值2'
+                Col0: 19,
+                Col1: 'test value 中文值2'
             }, {
                 returnContent: {
                     returnType: 'RT_NONE'
@@ -26,6 +33,38 @@ describe('#api', function () {
             }, function (err, result) {
                 should.ifError(err);
                 should.equal(result.consumed.capacityUnit.write, 1, 'capacityUnit.write shoud be 1');
+                should.equal(result.consumed.capacityUnit.read, 0, 'capacityUnit.read shoud be 0');
+                done();
+            });
+        });
+
+        it('Put Row with pk=testKey', function(done) {
+            ots.PutRow('sampleTable', {
+                pk: 'testKey'
+            }, {
+                Col0: 288,
+                Col10: 'hello ots'
+            }, null, function (err, result) {
+                should.ifError(err);
+                should.equal(result.consumed.capacityUnit.write, 1, 'capacityUnit.write shoud be 1');
+                should.equal(result.consumed.capacityUnit.read, 0, 'capacityUnit.read shoud be 0');
+                done();
+            });
+        });
+    });
+
+    describe('#UpdateRow', function() {
+        it('Update Row identify by pk with pkValue', function(done) {
+            ots.UpdateRow('sampleTable', {
+                pk: 'pkValue'
+            }, {
+                Col0: 20,
+                isRead: true,
+                debet: -10
+            }, null, function(err, result) {
+                should.ifError(err);
+                should.equal(result.consumed.capacityUnit.write, 1, 'capacityUnit.write shoud be 1');
+                should.equal(result.consumed.capacityUnit.read, 1, 'capacityUnit.read shoud be 1');
                 done();
             });
         });
@@ -34,16 +73,70 @@ describe('#api', function () {
     describe('#GetRow', function() {
         it('Get Row identify by pk with pkValue', function(done) {
             ots.GetRow('sampleTable',{
-                'pk': 'pkValue'
+                pk: 'pkValue'
             }, {
                 maxVersions: 1
             }, function (err, result) {
                 should.ifError(err);
                 should.equal(result.consumed.capacityUnit.read, 1, 'capacityUnit.read shoud be 1');
                 should.deepEqual(result.rowDecode, {
-                    pk: { pk: 'pkValue' },
-                    attr: { Col0: 19, Col1: 'test value 中文值2' }
+                    pk: {
+                        pk: 'pkValue'
+                    },
+                    attr: {
+                        Col0: 20,
+                        Col1: 'test value 中文值2',
+                        isRead: true,
+                        debet: -10
+                    }
                 });
+                done();
+            });
+        });
+    });
+
+    describe('#GetRange', function() {
+        it('GetRange order z->a', function(done) {
+            ots.GetRange('sampleTable', {
+                inclusiveStartPrimaryKey: {
+                    pk: 'z'
+                },
+                exclusiveEndPrimaryKey: {
+                    pk: 'a'
+                }
+            }, function(err, result) {
+                should.ifError(err);
+                should.equal(result.rowsDecode[0].pk.pk, 'testKey');
+                should.equal(result.rowsDecode[1].pk.pk, 'pkValue');
+                done();
+            });
+        });
+
+        it('GetRange INF_MIN->INF_MAX', function (done) {
+            ots.GetRange('sampleTable', {
+                inclusiveStartPrimaryKey: {
+                    pk: ots.INF_MIN
+                },
+                exclusiveEndPrimaryKey: {
+                    pk: ots.INF_MAX
+                }
+            }, function(err, result) {
+                should.ifError(err);
+                should.equal(result.rowsDecode[0].pk.pk, 'pkValue');
+                should.equal(result.rowsDecode[1].pk.pk, 'testKey');
+                done();
+            });
+        });
+    });
+
+    describe('#DeleteRow', function() {
+        it('DeleteRow Row identify by pk with pkValue', function(done) {
+            ots.DeleteRow('sampleTable', {
+                pk: 'pkValue'
+            }, null, function(err, result) {
+                should.ifError(err);
+                should.equal(result.consumed.capacityUnit.write, 1, 'capacityUnit.write shoud be 1');
+                should.equal(result.consumed.capacityUnit.read, 1, 'capacityUnit.read shoud be 1');
                 done();
             });
         });
